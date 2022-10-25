b1adf8a4681ce97ce71a51ff403ce5686786c307
isabellabagnulo <isabella.bagnulo@beije.it>
2022-10-25 10:57:18 +0200

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React, { FC } from "react"
-import logo from "./logo.svg"
+import { Routes, Route } from "react-router-dom"
+
+// Screens
+import About from "./screens/About"
+import Article from "./screens/Article"
+import Blog from "./screens/Blog"
+import Donate from "./screens/Donate"
+import Events from "./screens/Events"
+import Faq from "./screens/Faq"
+import Home from "./screens/Home"
+import Login from "./screens/Login"
+import NotFound from "./screens/NotFound"
+import PersonalArea from "./screens/PersonalArea"
+import SignUp from "./screens/Signup"
+import Support from "./screens/Support"
+
+import SCREENS from "./route/router"
+
 import "./App.css"
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route path={SCREENS.home} element={<Home />} />
+        <Route path={SCREENS.about} element={<About />} />
+        <Route path={SCREENS.article} element={<Article />} />
+        <Route path={SCREENS.blog} element={<Blog />} />
+        <Route path={SCREENS.donate} element={<Donate />} />
+        <Route path={SCREENS.events} element={<Events />} />
+        <Route path={SCREENS.faq} element={<Faq />} />
+        <Route path={SCREENS.login} element={<Login />} />
+        <Route path={SCREENS.personalArea} element={<PersonalArea />} />
+        <Route path={SCREENS.signUp} element={<SignUp />} />
+        <Route path={SCREENS.support} element={<Support />} />
+        <Route path={SCREENS.notFound} element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
