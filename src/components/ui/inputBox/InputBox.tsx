6ead9751791b8f7b496b7e2be935884fab707b46
marcoottaiano <marco.ottaiano@beije.it>
2022-10-26 16:00:37 +0200

    fix prefooter

--- src/components/ui/inputBox/InputBox.tsx
+++ src/components/ui/inputBox/InputBox.tsx
@@ -1,34 +1,34 @@
-import React, { FC, useState } from "react"
+import React, { FC, useState } from "react";
 // mui components
-import OutlinedInput from "@mui/material/OutlinedInput"
-import TextField, { TextFieldProps } from "@mui/material/TextField"
-import InputAdornment from "@mui/material/InputAdornment"
-import IconButton from "@mui/material/IconButton"
-import Visibility from "@mui/icons-material/Visibility"
-import VisibilityOff from "@mui/icons-material/VisibilityOff"
-import InputLabel from "@mui/material/InputLabel"
-import FormControl from "@mui/material/FormControl"
-import { createTheme, ThemeProvider } from "@mui/material/styles"
+import OutlinedInput from "@mui/material/OutlinedInput";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 interface InputBoxProps {
-  label: string
-  type: string
-  defaultValue?: string
-  isRequired?: boolean
-  error?: boolean
-  callbackChange?: Function
-  notValid?: boolean
+  label: string;
+  type: string;
+  defaultValue?: string;
+  isRequired?: boolean;
+  error?: boolean;
+  callbackChange?: Function;
+  notValid?: boolean;
 }
 
 interface State {
-  password: string
-  showPassword: boolean
+  password: string;
+  showPassword: boolean;
 }
 
 const initialState = {
   password: "",
   showPassword: false,
-}
+};
 
 const theme = createTheme({
   palette: {
@@ -37,43 +37,43 @@ const theme = createTheme({
       contrastText: "#fff",
     },
   },
-})
+});
 
 declare module "@mui/material/styles" {
   interface Palette {
-    neutral: Palette["primary"]
+    neutral: Palette["primary"];
   }
   interface PaletteOptions {
-    neutral?: PaletteOptions["primary"]
+    neutral?: PaletteOptions["primary"];
   }
 }
 
 const InputBox: FC<InputBoxProps> = (props) => {
-  const [state, setState] = useState<State>(initialState)
+  const [state, setState] = useState<State>(initialState);
 
   const change = (): void => {
     if (!!props.callbackChange) {
-      props.callbackChange()
+      props.callbackChange();
     }
-  }
+  };
 
   const handleClickShowPassword = () => {
     setState({
       ...state,
       showPassword: !state.showPassword,
-    })
-  }
+    });
+  };
 
   const handleMouseDownPassword = (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
-    event.preventDefault()
-  }
+    event.preventDefault();
+  };
 
   return (
     <ThemeProvider theme={theme}>
       {props.type === "password" ? (
-        <FormControl variant="outlined">
+        <FormControl variant="outlined" sx={{ width: "100%" }}>
           <InputLabel htmlFor="outlined-adornment-password" size="small">
             {props.label}
           </InputLabel>
@@ -103,6 +103,7 @@ const InputBox: FC<InputBoxProps> = (props) => {
         </FormControl>
       ) : (
         <TextField
+          sx={{ width: "100%" }}
           label={props.label}
           type={props.type}
           size="small"
@@ -114,7 +115,7 @@ const InputBox: FC<InputBoxProps> = (props) => {
         />
       )}
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default InputBox
+export default InputBox;
--- src/screens/Donate.tsx
+++ src/screens/Donate.tsx
@@ -1,34 +1,44 @@
-import { FC } from "react"
-import { useTranslation } from "react-i18next"
+import { FC } from "react";
+import { useTranslation } from "react-i18next";
 
 //components
-import Footer from "../components/footer/Footer"
+import Footer from "../components/footer/Footer";
+import Header from "../components/hooks/Header/Header";
+import PreFooter from "../components/preFooter/PreFooter";
+import InputBox from "../components/ui/inputBox/InputBox";
 
 //MUI
 
 //style
-import '../styles/donate.scss'
+import "../styles/donate.scss";
 
 const Donate: FC = () => {
-  const { t }: any = useTranslation()
-
+  const { t }: any = useTranslation();
 
   return (
     <>
-      <div className='donateContainer'>
-        <div className='titleDonate'>{t("personalArea.donate")}</div>
-        <section className='personalData'>
-          <div className='titlePersonalData'>{t("personalArea.personalData")}</div>
-        </section>
-        <section className='payment'>
-
-        </section>
-
-      </div>
+      <Header />
+      <main className="donateContainer">
+        <div className="titleDonate">{t("personalArea.donate")}</div>
+        <form action="">
+          <section className="personalData">
+            <div className="titlePersonalData">
+              {t("personalArea.personalData")}
+            </div>
+            <InputBox label={t("login.name")} type="text" isRequired={true} />
+            <InputBox
+              label={t("login.surname")}
+              type="text"
+              isRequired={true}
+            />
+          </section>
+          <section className="payment"></section>
+        </form>
+      </main>
+      <PreFooter />
       <Footer />
     </>
+  );
+};
 
-  )
-}
-
-export default Donate
+export default Donate;
