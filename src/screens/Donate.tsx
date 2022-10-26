a2a551682fd2711cff539e5a49b494a75093c873
pater97 <giuseppepater997@gmail.com>
2022-10-26 15:27:07 +0200

    bug json

--- src/screens/Donate.tsx
+++ src/screens/Donate.tsx
@@ -1,7 +1,34 @@
-import React from "react"
+import { FC } from "react"
+import { useTranslation } from "react-i18next"
 
-function Donate() {
-  return <div>Donate</div>
+//components
+import Footer from "../components/footer/Footer"
+
+//MUI
+
+//style
+import '../styles/donate.scss'
+
+const Donate: FC = () => {
+  const { t }: any = useTranslation()
+
+
+  return (
+    <>
+      <div className='donateContainer'>
+        <div className='titleDonate'>{t("personalArea.donate")}</div>
+        <section className='personalData'>
+          <div className='titlePersonalData'>{t("personalArea.personalData")}</div>
+        </section>
+        <section className='payment'>
+
+        </section>
+
+      </div>
+      <Footer />
+    </>
+
+  )
 }
 
 export default Donate
--- src/screens/Events.tsx
+++ src/screens/Events.tsx
@@ -1,7 +1,30 @@
 import React from "react"
+// componenti
+import Footer from "../components/footer/Footer"
+import PreFooter from "../components/preFooter/PreFooter"
+
+// mokup eventi
+const EVENTI:Array<object> = [
+  {
+    title:'',
+    image:'',
+    description:'',
+    requirement:'',
+    date:'',
+    time:'',
+    place:''
+  }
+]
 
 function Events() {
-  return <div>Events</div>
+  return(
+   
+    <div id="events">
+      <PreFooter/>
+      <Footer/>
+    </div>
+   
+  ) 
 }
 
 export default Events
