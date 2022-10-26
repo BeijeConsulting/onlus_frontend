d8f185dedc4401fe0ec1f52ac66f099531c99232
gianTrm <gianluca.tramontano@beije.it>
2022-10-26 17:57:58 +0200

    aggiornate le NavTab e lo screen PersonalArea

--- src/screens/PersonalArea.tsx
+++ src/screens/PersonalArea.tsx
@@ -1,7 +1,51 @@
-import React from "react"
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useMediaQuery } from "react-responsive";
+
+import Footer from "../components/footer/Footer";
+import Header from "../components/hooks/Header/Header";
+import NavTab from "../components/ui/NavTab/NavTab";
+import VerticalNavTab from "../components/ui/VerticalNavTab/VerticalNavTab";
+import PreFooter from "../components/preFooter/PreFooter";
+
+import "../styles/personalArea.scss";
 
 function PersonalArea() {
-  return <div>PersonalArea</div>
+  const { t }: any = useTranslation();
+  const isDesktop = useMediaQuery({ minWidth: "991px" });
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section className="welcomeCard">
+          <div className="icon" />
+          <h3>{t("personalArea.welcome")}</h3>
+        </section>
+        {isDesktop ? (
+          <VerticalNavTab
+            pages={[
+              t("personalArea.myInfo"),
+              t("nav.events"),
+              t("personalArea.donations"),
+            ]}
+            children={[<Header />, <Header />, <Header />]}
+          />
+        ) : (
+          <NavTab
+            pages={[
+              t("personalArea.myInfo"),
+              t("nav.events"),
+              t("personalArea.donations"),
+            ]}
+            children={[<Header />, <Header />, <Header />]}
+          />
+        )}
+        <PreFooter />
+        <Footer />
+      </main>
+    </>
+  );
 }
 
-export default PersonalArea
+export default PersonalArea;
