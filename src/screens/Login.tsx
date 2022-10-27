aa72cfea60396bd2a33c29d5329a43aedea02def
isabellabagnulo <isabella.bagnulo@beije.it>
2022-10-27 09:51:10 +0200

    upload login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,22 +1,22 @@
 //Components
-import Footer from "../components/footer/Footer";
-import Header from "../components/hooks/Header/Header";
-import CustomButton from "../components/ui/buttons/CustomButton/CustomButton";
-import InputBox from "../components/ui/inputBox/InputBox";
+import Footer from "../components/footer/Footer"
+import Header from "../components/hooks/Header/Header"
+import CustomButton from "../components/ui/buttons/CustomButton/CustomButton"
+import InputBox from "../components/ui/inputBox/InputBox"
 
 //Router
-import SCREENS from "../route/router";
-import { Link } from "react-router-dom";
+import SCREENS from "../route/router"
+import { Link } from "react-router-dom"
 
-//i18n 
+//i18n
 import { useTranslation } from "react-i18next"
 
 //Styles
-import '../styles/login.scss'
+import "../styles/login.scss"
 
 function Login() {
   const { t }: any = useTranslation()
-  const login=()=>{
+  const login = () => {
     console.log("login")
   }
 
@@ -31,10 +31,16 @@ function Login() {
           <InputBox label={t("login.email")} type={"mail"} />
           <InputBox label={t("login.password")} type={"password"} />
 
-          <a href="#" className="forgot-password">{t("login.forgottenPassword")}</a>
+          <a href="#" className="forgot-password">
+            {t("login.forgottenPassword")}
+          </a>
 
-          <CustomButton size={"big"} txtColor={"white"}  callback={login} bgColor={"#B12009"} label={t("buttons.loginButton")}/>
-
+          <CustomButton
+            size={"big"}
+            callback={login}
+            colorType="primary"
+            label={t("buttons.loginButton")}
+          />
         </form>
 
         <div className="aside-section">
@@ -45,7 +51,7 @@ function Login() {
 
       <Footer />
     </>
-  );
+  )
 }
 
-export default Login;
+export default Login
