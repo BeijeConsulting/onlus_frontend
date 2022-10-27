4d51913801e1b20d2f66832fdc9ffdf44355f688
pater97 <giuseppepater997@gmail.com>
2022-10-27 12:41:12 +0200

    card evenets completed

--- src/components/cardEvents/CardEvents.tsx
+++ src/components/cardEvents/CardEvents.tsx
@@ -10,6 +10,8 @@ import { CardHeader } from "@mui/material";
 import { useTranslation } from "react-i18next"
 // navigazione
 import { useNavigate } from 'react-router-dom';
+// componenti
+import CustomButton from "../ui/buttons/CustomButton/CustomButton";
 
 // props
 interface CardProps {
@@ -29,6 +31,11 @@ const CardEvents: FC<CardProps> = (props) => {
     // tranlation hook
     const { t }: any = useTranslation()
 
+    const goToBooking = ():void => {
+        // navigate('/login')
+        console.log('book')
+    }
+
     return (
         <Card sx={{
             display: "flex",
@@ -40,42 +47,43 @@ const CardEvents: FC<CardProps> = (props) => {
             {/* cambiare la scrollbar e finire data section */}
             <div className="cardContainer" style={{ display: 'flex' }}>
                 {/* 25 */}
-                <figure style={{ width: '30%' }}>
-                    <figcaption style={{ height: "15%" }}>
+                <figure style={{ width: '30%',display:"flex",flexDirection:"column" }}>
+                    <figcaption style={{ height: "20%" }}>
                         <CardHeader
-                            sx={{padding: "0"}}
+                            sx={{ padding: "0"}}
+                            titleTypographyProps={{fontWeight:700}}
                             title={props.title}
                         />
                     </figcaption>
-                    <div style={{ height: "94%" }}>
+                    <div style={{ height: "80%" }}>
                         <CardMedia
                             component="img"
-                            sx={{ width: '100%', height: "90%",marginTop:'auto' }}
+                            sx={{ width: '100%', height: "100%", marginTop: 'auto' }}
                             image={props.image}
                             alt="Live from space album cover"
                         />
                     </div>
                 </figure>
                 {/* 65 */}
-                <section className="details" style={{ width: '58%',overflowY:"auto" }}>
+                <section className="details" style={{ width: '58%', overflowY: "auto" }}>
                     <Box sx={{ display: "flex", flexDirection: "column" }}>
-                        <CardContent sx={{ flex: "1 0 auto",paddingX: "20px",paddingY:"0" }}>
-                            <Typography style={{marginBottom:'10px'}} component="div" variant="h5">
-                                Descrizione
+                        <CardContent sx={{ flex: "1 0 auto", paddingX: "20px", paddingY: "0" }}>
+                            <Typography style={{ marginBottom: '22px',fontWeight:700 }} component="div" variant="h5">
+                                {t("events.description")}
                             </Typography>
                             <Typography
                                 variant="subtitle1"
-                                color="text.secondary"
+                                color="text.primary"
                                 component="div"
                             >
                                 {props.description}
                             </Typography>
-                            <Typography component="div" variant="h5" sx={{ marginTop: "15px",marginBottom:'10px' }}>
-                                Requisiti
+                            <Typography component="div" variant="h5" sx={{ marginTop: "15px", marginBottom: '22px',fontWeight:700 }}>
+                                {t("events.requirements")}
                             </Typography>
                             <Typography
                                 variant="subtitle1"
-                                color="text.secondary"
+                                color="text.primary"
                                 component="div"
                             >
                                 {props.requirement}
@@ -84,43 +92,50 @@ const CardEvents: FC<CardProps> = (props) => {
                     </Box>
                 </section>
                 {/* 10 */}
-                <section style={{ width: '17%',display: "flex", flexDirection: "column",justifyContent:"space-between"  }}>
-                        <CardContent sx={{ flex: "1 0 auto", textAlign: "end" }}>
-                            <Typography
-                                component="div"
-                                variant="subtitle2"
-                                sx={{ fontSize: "18px" }}
-                            >
-                                {props.date}
-                            </Typography>
-                            <Typography
-                                variant="subtitle1"
-                                color="text.secondary"
-                                component="div"
-                            >
-                                {props.time}
-                            </Typography>
-                            <Typography
-                                variant="subtitle1"
-                                color="text.secondary"
-                                component="div"
-                            >
-                                {props.place}
-                            </Typography>
-                        </CardContent>
-                        <Box
-                            sx={{
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "flex-end",
-                                pl: 1,
-                                pb: 1
-                            }}
+                <section style={{ width: '17%', display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+                    <CardContent sx={{ flex: "1 0 auto", textAlign: "end"}}>
+                        <Typography
+                            component="div"
+                            variant="subtitle2"
+                            color="text.secondary"
+                            sx={{ fontSize: "15px",fontWeight:700}}
                         >
-                            <Button sx={{ width: "90px" }} variant="contained">
-                                {t("buttons.bookButton")}
-                            </Button>
-                        </Box>
+                            {props.date}
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            color="text.secondary"
+                            component="div"
+                            sx={{ fontSize: "15px",fontWeight:700}}
+                        >
+                            {props.time}
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            color="text.secondary"
+                            component="div"
+                            sx={{ fontSize: "15px",fontWeight:700}}
+                        >
+                            {props.place}
+                        </Typography>
+                    </CardContent>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "flex-end",
+                            pl: 1,
+                            pb: 1
+                        }}
+                    >
+                        <CustomButton
+                            colorType="secondary"
+                            callback={goToBooking}
+                            label={t("buttons.bookButton")}
+                            size={"small"}
+                        />
+
+                    </Box>
                 </section>
             </div>
         </Card>
