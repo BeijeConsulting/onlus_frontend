eb76079dc45c252803162e075b2c9a803d9f2488
Daniele Zappalà <daniele.zappala@beije.it>
2022-10-27 15:50:27 +0200

    update about

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,31 +1,95 @@
-
+import { useState, useEffect, FC } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 //Components
-import Footer from "../components/footer/Footer"
-import Header from "../components/hooks/Header/Header"
-import Hero from "../components/hooks/Hero/Hero"
-import PreFooter from "../components/preFooter/PreFooter"
+import Footer from "../components/footer/Footer";
+import Header from "../components/hooks/Header/Header";
+import Hero from "../components/hooks/Hero/Hero";
+import JoinUs from "../components/hooks/joinUsBbox/JoinUsBox";
+import PreFooter from "../components/preFooter/PreFooter";
 
 //Styles
-import "../styles/about.scss"
+import "../styles/about.scss";
 
-function About() {
-  return(
+interface State {
+  content: Array<content>;
+}
+type content = {
+  text: string;
+  img: string;
+};
+
+const initialState: State = {
+  content: [],
+};
+const About: FC =() =>{
+  const { t }: any = useTranslation();
+  const navigate: any = useNavigate();
+  const [state, setState] = useState<State>(initialState);
+
+  useEffect(() => {
+    fetchDatas();
+  }, []);
+
+  function handleNavigate() {
+    navigate("/donate");
+  }
+
+  async function fetchDatas() {
+    let result: any = await axios.get("mockAPI/about.json");
+    console.log("result axios: ", result.data.content);
+    setState({content:result.data.content})
+  }
+  const mappingContent=(item:any,key:any)=>{
+    if(key%2===0){
+      return(
+        <section className="content-about-container" key={key}>
+          <div className="text-about">{item.text}</div>
+          <div className="img-container">
+            <img
+              className="img-about"
+              src={require(`../assets/images/${item.image}`)}
+              alt="hero-img"
+            />
+          </div>
+        </section>
+      )
+    }else{
+      return(
+        <section className="content-about-container-inverted" key={key}>
+          <div className="text-about">{item.text}</div>
+          <div className="img-container-inverted">
+            <img
+              className="img-about"
+              src={require(`../assets/images/${item.image}`)}
+              alt="hero-img"
+            />
+          </div>
+        </section>
+      )
+    }   
+  }
+  
+  return (
     <>
       <Header />
 
       <Hero />
       <main>
-        <h1>Chi siamo</h1>
-        <section>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem iusto aliquam exercitationem velit odit maiores, natus architecto veritatis hic rerum delectus tempora, officia soluta sit sapiente! Accusamus id obcaecati quas.</section>
+        <h1>{t("nav.about")}</h1>
+        {state.content.map(mappingContent)}
       </main>
-      
-
+      <JoinUs />
 
       <PreFooter />
       <Footer />
     </>
-  )
+  );
 }
 
-export default About
+export default About;
+/*
+
+*/
