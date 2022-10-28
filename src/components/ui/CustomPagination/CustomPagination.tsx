354562b9f955123679a4142b4a9b40abb05b0487
GianlucaTramontano <82704255+Gianbho@users.noreply.github.com>
2022-10-28 12:12:33 +0200

    added CustomPagination.tsx

---
+++ src/components/ui/CustomPagination/CustomPagination.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+interface LocalProps {
+    callbackChange: Function
+}
+
+function CustomPagination(props:LocalProps):React.ReactElement {
+  const [page, setPage] = React.useState(1);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+    props.callbackChange(value)
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Typography>Page: {page}</Typography>
+      <Pagination count={10} page={page} onChange={handleChange} />
+    </Stack>
+  );
+}
+
+export default CustomPagination
