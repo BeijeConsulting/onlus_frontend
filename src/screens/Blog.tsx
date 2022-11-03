177e1dd01f56a8ae02c3cff6d6255d39ba1b493b
isabellabagnulo <isabella.bagnulo@beije.it>
2022-11-03 14:28:22 +0100

    update

--- src/screens/Blog.tsx
+++ src/screens/Blog.tsx
@@ -1,76 +1,76 @@
-import { FC, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { Typography } from "@mui/material";
+import { FC, useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
+import { Typography } from "@mui/material"
 
-import Footer from "../components/hooks/Footer/Footer";
-import Header from "../components/hooks/Header/Header";
-import InputBox from "../components/hooks/inputBox/InputBox";
-import SelectBox from "../components/hooks/inputBox/SelectBox";
-import PreFooter from "../components/hooks/preFooter/PreFooter";
+import Footer from "../components/hooks/Footer/Footer"
+import Header from "../components/hooks/Header/Header"
+import InputBox from "../components/hooks/inputBox/InputBox"
+import SelectBox from "../components/hooks/inputBox/SelectBox"
+import PreFooter from "../components/hooks/preFooter/PreFooter"
 
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet"
 
 //utils
-import { articles, categories } from "../utils/data";
-import { article, category } from "../utils/type";
+import { articles, categories } from "../utils/data"
+import { article, category } from "../utils/type"
 
 //style
-import "../styles/blog.scss";
-import CardArticle from "../components/ui/CardArticle/CardArticle";
-import { useMediaQuery } from "react-responsive";
-import { useNavigate } from "react-router-dom";
-import SCREENS from "../route/router";
+import "../styles/blog.scss"
+import CardArticle from "../components/ui/CardArticle/CardArticle"
+import { useMediaQuery } from "react-responsive"
+import { useNavigate } from "react-router-dom"
+import SCREENS from "../route/router"
 
 interface State {
-  categories: Array<category>;
-  articles: Array<article>;
+  categories: Array<category>
+  articles: Array<article>
 }
 
 const initialState = {
   categories: [],
   articles: articles,
-};
+}
 
 //React responsive const
 const Default = ({ children }: any) => {
-  const isNotMobile = useMediaQuery({ minWidth: 992 });
-  return isNotMobile ? children : null;
-};
+  const isNotMobile = useMediaQuery({ minWidth: 992 })
+  return isNotMobile ? children : null
+}
 const Mobile = ({ children }: any) => {
-  const isMobile = useMediaQuery({ maxWidth: 991 });
-  return isMobile ? children : null;
-};
+  const isMobile = useMediaQuery({ maxWidth: 991 })
+  return isMobile ? children : null
+}
 
-let localArticles: Array<article> = articles;
+let localArticles: Array<article> = articles
 
 const Blog: FC = () => {
-  const navigate: Function = useNavigate();
-  const [state, setState] = useState<State>(initialState);
-  const { t }: any = useTranslation();
+  const navigate: Function = useNavigate()
+  const [state, setState] = useState<State>(initialState)
+  const { t }: any = useTranslation()
 
   useEffect(() => {
-    createCategories();
-  }, []);
+    createCategories()
+  }, [])
 
   const createCategories = (): void => {
-    let localCategories: Array<category> = [];
+    let localCategories: Array<category> = []
     categories.forEach((e) => {
       let singleCategory: category = {
         label: e.name,
         value: e.name,
-      };
-      localCategories.push(singleCategory);
-    });
-    console.log(localCategories);
+      }
+      localCategories.push(singleCategory)
+    })
+    console.log(localCategories)
     setState({
       ...state,
       categories: localCategories,
-    });
-  };
+    })
+  }
 
   const goToArticle = (id: number) => (): void => {
-    navigate(SCREENS.article + `/${id}`);
-  };
+    navigate(SCREENS.article + `/${id}`)
+  }
 
   const mapping = (el: article, key: number): JSX.Element => {
     return (
@@ -94,24 +94,24 @@ const Blog: FC = () => {
           />
         </Default>
       </div>
-    );
-  };
+    )
+  }
 
   const search = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    let textInputValue: string = e.target.value;
+    let textInputValue: string = e.target.value
     let filteredArticles: Array<article> = localArticles.filter((obj) => {
-      return obj.title.toLowerCase().includes(textInputValue.toLowerCase());
-    });
-    console.log(filteredArticles);
+      return obj.title.toLowerCase().includes(textInputValue.toLowerCase())
+    })
+    console.log(filteredArticles)
     setState({
       ...state,
       articles: filteredArticles,
-    });
-  };
+    })
+  }
 
   const handleCategory = (): void => {
     //chiamata api
-  };
+  }
 
   return (
     <>
@@ -122,7 +122,7 @@ const Blog: FC = () => {
       <Header />
       <main id="blog" className="sectionContainer">
         <Typography variant="h1">{t("titles.blogTitle")}</Typography>
-        <section className="searchContainer">
+        <section className="searchContainer input-box">
           <InputBox label={t("search")} type="text" callbackChange={search} />
           <SelectBox
             label={t("selectCategory")}
@@ -137,7 +137,7 @@ const Blog: FC = () => {
       <PreFooter />
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Blog;
+export default Blog
