e0a09a467f07bb7631f96086cace555940f1d151
isabellabagnulo <isabella.bagnulo@beije.it>
2022-11-09 16:26:04 +0100

    fixed card event

--- src/components/hooks/CardEvents/CardEvents.tsx
+++ src/components/hooks/CardEvents/CardEvents.tsx
@@ -1,131 +1,110 @@
-import { FC } from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
+import { FC, useEffect, useRef, useState } from "react"
+
+// mui
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Typography from "@mui/material/Typography"
+
+import { MutableRefObject } from "react"
+
 // translation
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next"
 
 // componenti
-import CustomButton from "../../ui/buttons/CustomButton/CustomButton";
+import CustomButton from "../../ui/buttons/CustomButton/CustomButton"
 
 //Style
-import "./cardEvents.scss";
+import "./cardEvents.scss"
 
 // props
 interface CardProps {
-  title: string;
-  image: string;
-  description: string;
-  requirement: string;
-  date: string;
-  time: string;
-  place: string;
+  title: string
+  image: string
+  description: string
+  requirement: string
+  date: string
+  time: string
+  place: string
+}
+
+interface State {
+  height: number
+}
+
+const initialState = {
+  height: 0,
 }
 
 const CardEvents: FC<CardProps> = (props) => {
+  const [state, setState] = useState<State>(initialState)
+
   // tranlation hook
-  const { t }: any = useTranslation();
+  const { t }: any = useTranslation()
+
+  const headerRef: MutableRefObject<any> = useRef(0)
 
   const goToBooking = (): void => {
     // navigate('/login')
-    console.log("book");
-  };
+    console.log("book")
+  }
+
+  useEffect(() => {
+    setState({
+      ...state,
+      height: headerRef.current.clientHeight,
+    })
+  }, [])
 
   return (
-    <Card
-      sx={{
-        display: "flex",
-        // maxHeight: "400px",
-        padding: "40px",
-        justifyContent: "space-between",
-      }}
-    >
-      {/* cambiare la scrollbar e finire data section */}
-      <div className="cardContainer">
-        {/* 25 */}
-        <figure style={{ display: "flex", flexDirection: "column" }}>
-          <figcaption style={{ height: "20%", paddingBottom: "20px" }}>
-            <Typography variant="h3">{props.title}</Typography>
-          </figcaption>
-          <div style={{ maxHeight: "258.38px" }}>
-            <CardMedia
-              component="img"
-              sx={{ width: "100%", height: "100%", marginTop: "auto" }}
-              image={props.image}
-              alt="Live from space album cover"
-            />
-          </div>
-        </figure>
-        {/* 65 */}
-        <section className="details" style={{ overflowY: "auto" }}>
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <CardContent
-              sx={{
-                maxHeight: "360px",
-                flex: "1 0 auto",
-                paddingX: "20px",
-                paddingY: "0",
-                width: "100%",
-              }}
-            >
-              <Typography sx={{ marginBottom: "10px" }} variant="h4">
-                {t("events.description")}
-              </Typography>
-              <Typography variant="body1" color="text.primary" component="div">
-                {props.description}
-              </Typography>
-              <Typography
-                variant="h4"
-                sx={{
-                  marginTop: "20px",
-                  marginBottom: "10px",
-                }}
-              >
-                {t("events.requirements")}
-              </Typography>
-              <Typography variant="body1" color="text.primary" component="div">
-                {props.requirement}
-              </Typography>
-            </CardContent>
-          </Box>
-        </section>
-        {/* 10 */}
-        <section
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-          }}
+    <Card className="eventsCard">
+      <div className="cardHeader" ref={headerRef}>
+        <Typography variant="h3" className="cardTitle">
+          {props.title}
+        </Typography>
+        <CardMedia
+          className="cardMedia"
+          component="img"
+          image={props.image}
+          alt="Live from space album cover"
+        />
+      </div>
+
+      <CardContent className="cardContent">
+        <div
+          className="cardContentSection cardContentSectionScroll"
+          style={{ height: `${state.height}px` }}
         >
-          <CardContent sx={{ flex: "1 0 auto", textAlign: "end" }}>
+          <section>
+            <Typography variant="h4">{t("events.description")}</Typography>
+            <Typography variant="body1" color="text.primary" component="div">
+              {props.description}
+            </Typography>
+          </section>
+          <section>
+            <Typography variant="h4">{t("events.requirements")}</Typography>
+            <Typography variant="body1" color="text.primary" component="div">
+              {props.requirement}
+            </Typography>
+          </section>
+        </div>
+
+        <div className="cardContentSection cardContentSectionRight">
+          <div className="cardInfo">
             <Typography variant="body2">{props.date}</Typography>
             <Typography variant="body2">{props.time}</Typography>
             <Typography variant="body2">{props.place}</Typography>
-          </CardContent>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-              pl: 1,
-              pb: 1,
-            }}
-          >
-            <div style={{ paddingRight: 16 }}>
-              <CustomButton
-                colorType="secondary"
-                callback={goToBooking}
-                label={t("buttons.bookButton")}
-                size={"small"}
-              />
-            </div>
-          </Box>
-        </section>
-      </div>
+          </div>
+          <CustomButton
+            colorType="secondary"
+            callback={goToBooking}
+            label={t("buttons.bookButton")}
+            size={"small"}
+          />
+        </div>
+      </CardContent>
     </Card>
-  );
-};
+  )
+}
 
-export default CardEvents;
+export default CardEvents
