96a07ffd4b5a2f20629bbd460d266ba3bb132035
gianTrm <gianluca.tramontano@beije.it>
2022-11-14 12:01:55 +0100

    no message

--- src/services/api/authApi.js
+++ src/services/api/authApi.js
@@ -1,7 +1,7 @@
 import { postApi, postApiNoAuth } from "../genericServices";
 
 export async function signInApi(email, password) {
-    return await postApi('/signin', {
+    return await postApiNoAuth('/signin', {
         "email": email,
         "password": password
     })
